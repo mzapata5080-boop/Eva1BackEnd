cbc59cb035b56ae78f5f444384451b05096ed010
mzapata5080-boop <mzapata.5080@gmai.com>
2026-09-17 18:23:36 -0300

    App 1 terminada

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from app1 import views as app1_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', app1_views.inicio, name='inicio'),
+    path('app1/', include('app1.urls')),
 ]
